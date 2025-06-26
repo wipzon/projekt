7a38181067ab9b776849ceb7a33819e4479e32e4
wipzon <wiktorzolnierek@icloud.com>
2025-06-27 01:05:57 +0200

    ughhhhh

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './calculator.js',   // Adjust this to your real entry file
+  entry: './calculator.js', // or your main JS file
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // <-- tell Webpack where your HTML is
+    }),
+  ],
 };
